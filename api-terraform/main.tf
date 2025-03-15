ce7bf0165520f6ed14d97c3b9ebeaf6a111f0504
kgotlelelo1 <kgotlelelo.mangene@bbd.co.za>
2025-03-15 14:31:59 +0200

    updated vpc creation

--- api-terraform/main.tf
+++ api-terraform/main.tf
@@ -2,12 +2,51 @@ provider "aws" {
   region = "af-south-1"
 }
 
+resource "aws_vpc" "task_time_tracker_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "task-time-tracker-vpc"
+  }
+}
+
+resource "aws_subnet" "task_time_tracker_subnet" {
+  vpc_id                  = aws_vpc.task_time_tracker_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "af-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "task-time-tracker-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "task_time_tracker_igw" {
+  vpc_id = aws_vpc.task_time_tracker_vpc.id
+  tags = {
+    Name = "task-time-tracker-igw"
+  }
+}
+
+resource "aws_route_table" "task_time_tracker_route_table" {
+  vpc_id = aws_vpc.task_time_tracker_vpc.id
+}
+
+resource "aws_route" "task_time_tracker_route" {
+  route_table_id         = aws_route_table.task_time_tracker_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.task_time_tracker_igw.id
+}
+
+resource "aws_route_table_association" "task_time_tracker_route_table_association" {
+  subnet_id      = aws_subnet.task_time_tracker_subnet.id
+  route_table_id = aws_route_table.task_time_tracker_route_table.id
+}
+
 resource "aws_security_group" "task_time_tracker_sg" {
-  vpc_id      = "vpc-089eae23fbb59d327"
-  name        = "task-time-tracker-sg"
+  vpc_id      = aws_vpc.task_time_tracker_vpc.id
   description = "Allow inbound SSH and HTTP traffic"
   
-
   ingress {
     from_port   = 22
     to_port     = 22
@@ -35,14 +74,19 @@ resource "aws_security_group" "task_time_tracker_sg" {
     protocol    = "-1"  
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "task-time-tracker-sg"
+  }
 }
 
 resource "aws_instance" "task_time_tracker_instance" {
   ami           = "ami-00d6d5db7a745ff3f"  
   instance_type = "t3.micro"               
-  key_name      = "task_time_tracker_key_pair"  
+  key_name      = "task_time_tracker_key"  
 
-  security_groups = [aws_security_group.task_time_tracker_sg.name]
+  subnet_id     = aws_subnet.task_time_tracker_subnet.id
+  security_groups = [aws_security_group.task_time_tracker_sg.id]
 
   associate_public_ip_address = true
 
